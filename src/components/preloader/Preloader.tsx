0aeadbe3bba9d1bc5b94577066835ac70e783806
Mykola Osadchii <nikolayosadchii842@gmail.com>
2025-08-10 13:31:57 +0200

    add Preloader

---
+++ src/components/preloader/Preloader.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './Preloader.module.css';
+import preloaderImage from '../../assets/preloader2.gif'; // Adjust the path as necessary
+
+
+const Preloader = () => {
+  return (
+	<div className={style.main}>
+		<img src={preloaderImage} alt="Loading..."  className={style.preloaderImg}/>
+	</div>
+  );
+};
+
+export default Preloader;
--- src/layouts/mainLayout/MainLayout.tsx
+++ src/layouts/mainLayout/MainLayout.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import style from './MainLayout.module.css';
+import logo from '../../assets/logo.jpg'
 
 const MainLayout: React.FC = () => {
   return (
 	<div className={style.main}>
-	  <h2>Header</h2>
+	<div className={style.header}>
+	<img src={logo} alt="logo"  className={style.logo}/>
+	<h2>ArcanumAI</h2>
+	</div>
+		
 	  <Outlet/>
 	</div>
   );
