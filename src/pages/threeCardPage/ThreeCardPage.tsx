5b0be205eb7dd1272807d96183b79df6fdd1d685
Mykola Osadchii <nikolayosadchii842@gmail.com>
2025-08-13 18:19:04 +0200

    add vercel.json file

--- src/pages/threeCardPage/ThreeCardPage.tsx
+++ src/pages/threeCardPage/ThreeCardPage.tsx
@@ -13,8 +13,8 @@ const ThreeCardPage: React.FC = () => {
 	const [resault, setResault] = useState("");
 	const [isLoading, setIsLoading] = useState(false);
 	const [threeCards, setThreeCards] = useState([{id: 1, name: "", description: "", img: ""}, {id: 2, name: "", description: "", img: ""}, {id: 3, name: "", description: "", img: ""}]);
-	const URLprod = 'https://arcanumaibackend.onrender.com';
-	// const URLdev = 'http://localhost:3000';
+	// const URLprod = 'https://arcanumaibackend.onrender.com';
+	const URLdev = 'http://localhost:3000';
 
 	function questionFunction(event: React.ChangeEvent<HTMLInputElement>) {
 		setQuestion(event.target.value)
@@ -27,7 +27,7 @@ const ThreeCardPage: React.FC = () => {
 	}
 		const fetchData = async () => {
 			try {
-				const res = await axios.post(URLprod, {
+				const res = await axios.post(URLdev, {
 					question: question,
 				});
 				setThreeCards(res.data.threeCards);
